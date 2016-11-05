41985b0f797a28b39bb4d1ef7134f749e1ae9d7d
Dante <web@conex.club>
2016-11-04 20:09:18 -0500

    sending files

---
+++ public/javascripts/url.js
@@ -0,0 +1,66 @@
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+var newURL = getParameterByName('url');
+
+if (newURL) {
+	newURL = atob(newURL);
+}
+else{
+    newURL = "#"
+}
+
+var finalButton =  document.getElementById("finalButton");
+
+finalButton.href = newURL;
+
+
+
+
+var nameTitle = getParameterByName('campaing_title');
+document.getElementById("campaignTitle").innerHTML = nameTitle;
+
+console.log(nameTitle);
+
+var buttonTitle = getParameterByName('button_title');
+document.getElementById("finalButton").innerHTML = buttonTitle;
+
+var imgTitle = getParameterByName('img_title');
+
+if (imgTitle) {
+    imgTitle = atob(imgTitle);
+}
+else{
+    imgTitle = "#"
+}
+
+document.getElementById("imageInsert").src = imgTitle;
+
+
+
+// OG IMG FB:
+metaImg = $('meta[property="og:image"]')[0];
+metaImg.content = imgTitle;
+
+// OG TITLE FB:
+metaTitle = $('meta[property="og:title"]')[0];
+metaTitle.content = nameTitle;
+
+
+
+
+
+// click
+ function bodyHref(){
+    this.location.href= newURL;
+ };
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'SPINVER', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
+});
+
+
+router.get('/generate', function(req, res, next) {
+  res.render('generate');
+  
+});
+
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
+});
+
+module.exports = router;
